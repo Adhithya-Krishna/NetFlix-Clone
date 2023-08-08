e48f94e6953fca8ff0fe2cfeb35cde0caf0f62db
Adhithya Krishna S <adithyakrshn4@gmail.com>
2023-08-08 14:10:44 +0530

    Initial App

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <div className='navbar'>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png" alt="Netflix-Logo" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png?20201013161117" alt="Netflix-Avatar" />
+        </div>
+    )
+}
+
+export default NavBar
